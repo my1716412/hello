f4c3aae271cc7ba341e288148964ffb5fef3953b
User <User@YD03-05>
2019-09-27 11:01:49 +0900

    111

---
+++ HelloWorld/src/hello/Student.java
@@ -0,0 +1,61 @@
+package hello;
+
+public class Student {
+	private String name;
+	private String id;
+	private int kor;
+	private int eng;
+	private int math;
+
+	public Student() {
+
+	}
+
+	public Student(String name, String id, int kor, int eng, int math) {
+		this.name = name;
+		this.id = id;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+}
